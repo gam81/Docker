f84c877cfe7579aa080ea55464c1944c50030320
Giovanni Meiser <70547206+gam81@users.noreply.github.com>
2021-10-26 16:21:50 -0400

    fix aws keys

--- HW3/cloud.py
+++ HW3/cloud.py
@@ -1,15 +1,15 @@
 import boto3
 s3 = boto3.resource('s3', 
-    aws_access_key_id='AKIAYQ6YHT4IHYVAJGOV', 
-    aws_secret_access_key='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' 
+    aws_access_key_id='key1', 
+    aws_secret_access_key='key2+' 
 ) 
 
 bucket = s3.Bucket("meiser-bucket") 
 
 dyndb = boto3.resource('dynamodb', 
     region_name='us-east-2', 
-    aws_access_key_id='AKIAYQ6YHT4IHYVAJGOV', 
-    aws_secret_access_key='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' 
+    aws_access_key_id='key1', 
+    aws_secret_access_key='key2+' 
 ) 
 
 table = dyndb.Table("DataTable") 
