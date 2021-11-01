e8b3dedc64f5f18be0503986d1735104c5d6b257
Giovanni Meiser <70547206+gam81@users.noreply.github.com>
2021-10-31 23:44:57 -0400

    Add files via upload

---
+++ course checkpoint/mainClient.py.py
@@ -0,0 +1,9 @@
+import google.cloud.container_v1 as container
+
+from google.auth import compute_engine
+
+from google.cloud.container_v1 import ClusterManagerClient
+
+from kubernetes import client, config
+
+//connect to ClusterManagerClient.getCluster() and run code
